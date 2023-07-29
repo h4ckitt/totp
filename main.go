d62ee26e6bab99898b350f3c4ba8890bf67cf994
h4ckitt <dammysteve10@gmail.com>
2023-07-29 10:21:56 +0100

    [add] Added Support For TOTP

--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math"
+	"time"
 )
 
 func main() {
 	key := "12345678901234567890"
+	//interval := 30
 	// HOTP
 	fmt.Println(calculateOTP(key, 0))
+
+	// TOTP
+	fmt.Println(calculateOTP(key, getTimeStep(30)))
+
+	for range time.Tick(30 * time.Second) {
+		fmt.Println(calculateOTP(key, getTimeStep(30)))
+	}
 }
 
 func calculateOTP(key string, counter uint64) uint32 {
@@ -26,4 +35,8 @@ func calculateOTP(key string, counter uint64) uint32 {
 	hash[offset] = hash[offset] & 0x7f // mask the MSB to a zero to convert it to an unsigned int
 	data := binary.BigEndian.Uint32(hash[offset : offset+4])
 	return data % uint32(math.Pow(10, 6))
+}
+
+func getTimeStep(interval int64) uint64 {
+	return uint64(time.Now().Unix() / interval)
 }
